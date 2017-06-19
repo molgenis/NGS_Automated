35b8505566b6df85b15b317f4554446750542040
RoanKanninga <roankanninga@gmail.com>
2017-06-19 12:36:58 +0200

    possible locked file fix + changed order of copying data

--- copyProjectDataToPrm.sh
+++ copyProjectDataToPrm.sh
@@ -39,23 +39,23 @@ fi
 }
 module load hashdeep/4.4-foss-2015b
 
+        if [ -f ${LOGDIR}/copyProjectDataToPrm.sh.locked ]
+        then
+		continue
+	else
+		touch ${LOGDIR}/copyProjectDataToPrm.sh.locked
+        fi
 
 for line in ${ARR[@]}
 do
         projectName=${line}
 
         LOGGER=${LOGDIR}/${projectName}/${projectName}.copyProjectDataToPrm.logger
-	
-        FINISHED="no"
 
-        if [ -f ${LOGDIR}/copyProjectDataToPrm.sh.locked ]
-        then
-            	continue
-	else
-		touch ${LOGDIR}/copyProjectDataToPrm.sh.locked
-        fi
+	FINISHED="no"
+
 	##command to check if projectfolder exists
-	makeProjectDataDir=$(ssh ${groupname}-dm@calculon.hpc.rug.nl "sh ${PROJECTSDIRPRM}/checkProjectData.sh ${PROJECTSDIRPRM} ${projectName}")	
+	makeProjectDataDir=$(ssh ${groupname}-dm@calculon.hpc.rug.nl "sh ${PROJECTSDIRPRM}/checkProjectData.sh ${PROJECTSDIRPRM} ${projectName}")
 	copyProjectDataDiagnosticsClusterToPrm="${PROJECTSDIR}/${projectName}/* ${groupname}-dm@calculon.hpc.rug.nl:${PROJECTSDIRPRM}/${projectName}"
 
 	if [[ -f $LOGDIR/${projectName}/${projectName}.pipeline.finished && ! -f $LOGDIR/${projectName}/${projectName}.projectDataCopiedToPrm ]]
@@ -76,8 +76,8 @@ do
 		if [ "${makeProjectDataDir}" == "f" ]
 		then
 			echo "copying project data from DiagnosticsCluster to prm" >> ${LOGGER}
-                        rsync -r -av --exclude rawdata/ ${copyProjectDataDiagnosticsClusterToPrm} >> $LOGGER
 			rsync -r -av ${PROJECTSDIR}/${projectName}/${projectName}.allResultmd5sums ${groupname}-dm@calculon.hpc.rug.nl:${PROJECTSDIRPRM}/${projectName}/
+                        rsync -r -av --exclude rawdata/ ${copyProjectDataDiagnosticsClusterToPrm} >> $LOGGER
 			makeProjectDataDir="t"
 		fi
 		if [ "${makeProjectDataDir}" == "t" ]
@@ -85,7 +85,7 @@ do
                         countFilesProjectDataDirPrm=$(ssh ${groupname}-dm@calculon.hpc.rug.nl "ls -R ${PROJECTSDIRPRM}/${projectName}/*/results/ | wc -l")
                         if [ ${countFilesProjectDataDirTmp} -eq ${countFilesProjectDataDirPrm} ]
                         then
-  				echo "${countFilesProjectDataDirTmp} -eq ${countFilesProjectDataDirPrm}"
+				echo "${countFilesProjectDataDirTmp} -eq ${countFilesProjectDataDirPrm}"
                                 COPIEDTOPRM=$(ssh ${groupname}-dm@calculon.hpc.rug.nl "sh ${PROJECTSDIRPRM}/check.sh ${PROJECTSDIRPRM} ${projectName}")
 				if [[ "${COPIEDTOPRM}" == *"FAILED"* ]]
                                 then
@@ -98,7 +98,7 @@ do
 					echo "finished copying project data to calculon" >> ${LOGGER}
 					printf "De project data voor project ${projectName} is gekopieerd naar ${PROJECTSDIRPRM}" | mail -s "project data for project ${projectName} is copied to permanent storage" ${ONTVANGER}
 
-				  	if [ -f $LOGDIR/${projectName}/${projectName}.copyProjectDataToPrm.failed ] 
+					if [ -f $LOGDIR/${projectName}/${projectName}.copyProjectDataToPrm.failed ] 
                                         then
 						rm $LOGDIR/${projectName}/${projectName}.copyProjectDataToPrm.failed
 					fi
@@ -119,8 +119,9 @@ do
 			printf "De md5sum checks voor project ${projectName} op ${PROJECTSDIRPRM} zijn mislukt.De originele data staat op ${HOSTNA}:${PROJECTSDIR}\n\nDeze mail is verstuurd omdat er al 10 pogingen zijn gedaan om de data te kopieren/md5summen" | mail -s "${projectName} failing to copy to permanent storage" ${ONTVANGER}
 		fi
 	fi
-	rm ${LOGDIR}/copyProjectDataToPrm.sh.locked
 done
+
+rm ${LOGDIR}/copyProjectDataToPrm.sh.locked
 
 trap - EXIT
 exit 0
