3ac5a296f4452b367d32ec43f52aeb41ef06ff3a
gerbenvandervries <gerbenvandervries@gmail.com>
2018-01-23 16:21:38 +0100

    auto detect sampleType, for trace and trace

--- bin/startPipeline.sh
+++ bin/startPipeline.sh
@@ -180,7 +180,7 @@ function submitPipeline () {
 	#
 	local _url="https://${MOLGENISSERVER}/menu/track&trace/dataexplorer?entity=status_jobs&mod=data&query%5Bq%5D%5B0%5D%5Boperator%5D=SEARCH&query%5Bq%5D%5B0%5D%5Bvalue%5D=${_project}"
 	printf '%s\n' "project,run_id,pipeline,url,copy_results_prm,date"  > "${_controlFileBase}.trackAndTrace.csv"
-	printf '%s\n' "${_project},${_project},DNA,${_url},,"             >> "${_controlFileBase}.trackAndTrace.csv"
+	printf '%s\n' "${_project},${_project},${_sampleType},${_url},,"             >> "${_controlFileBase}.trackAndTrace.csv"
 	trackAndTracePostFromFile 'status_projects' 'add'                    "${_controlFileBase}.trackAndTrace.csv"
 	
 	_url="https://${MOLGENISSERVER}/menu/track&trace/dataexplorer?entity=status_samples&hideselect=true&mod=data&query%5Bq%5D%5B0%5D%5Boperator%5D=SEARCH&query%5Bq%5D%5B0%5D%5Bvalue%5D=${_project}"
