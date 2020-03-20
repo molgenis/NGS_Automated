195cfc971525decd8e5f46bf4f46eb3801e99dc5
RoanKanninga <roankanninga@gmail.com>
2020-03-20 12:16:24 +0100

    fixed issue based on feedback

--- bin/copyRawDataToPrm.sh
+++ bin/copyRawDataToPrm.sh
@@ -608,7 +608,7 @@ else
 		# Process this sample sheet / run.
 		#	
 		fileP="$(basename "${sampleSheet}")"
-		filePrefix="$("${fileP%.*}")"
+		filePrefix="${fileP%.*}"
 		log4Bash 'INFO' "${LINENO}" "${FUNCNAME:-main}" '0' "Processing run ${filePrefix} ..."
 		#
 		# ToDo: change location of log files back to ${TMP_ROOT_DIR} once we have a 
