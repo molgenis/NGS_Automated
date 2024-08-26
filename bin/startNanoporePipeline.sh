14eea4b099c95ccdbbed3cf3e84f8a76b2bbe93a
dennishendriksen <d.hendriksen@umcg.nl>
2024-08-26 05:50:38 +0000

    update VIP 7.8.0 to 7.9.0

--- bin/startNanoporePipeline.sh
+++ bin/startNanoporePipeline.sh
@@ -125,7 +125,7 @@ function executeVip () {
 	# step 2: execute vip
 	#
 	log4Bash 'DEBUG' "${LINENO}" "${FUNCNAME[0]:-main}" '0' "execute VIP"
-	local -r _vip_version="v7.8.0"
+	local -r _vip_version="v7.9.0"
 	local -r _vip_dir="${_pipeline_software_dir}/vip/${_vip_version}"
 	local -r _vip_config_file="${_pipeline_software_dir}/resources/run.cfg"
 	local -r _vip_output_dir="${TMP_ROOT_DIR}/projects/nanopore/${_project}/${_run}/results"
